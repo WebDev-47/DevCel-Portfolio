330938098b625e7c05e70ede8cf1a9287430456a
web-dev <webdev6190@gmail.com>
2024-11-13 04:24:52 -0800

    Updated Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
-
 import { Route, Routes } from 'react-router-dom'
 import Home from './Components/Home'
 import Projects from './Components/Projects'
-
 import About from './Components/About'
 import Contact from './Components/Contact'
 import Footer from './Components/Footer'
 import Hero from './Components/Hero'
-
 import Services from './Components/Services'
 import Navba from './Components/Navba'
 
